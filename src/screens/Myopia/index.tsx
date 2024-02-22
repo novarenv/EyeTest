eb0b3243c250776a5a3ab44b271fc8f8b045ec15
Moch. AKbar Ilham <mochakbarilham@gmail.com>
2024-02-22 21:54:54 +0700

    Perubahan Pengecekkan

--- src/screens/Myopia/index.tsx
+++ src/screens/Myopia/index.tsx
@@ -1,20 +1,13 @@
 /* eslint-disable prettier/prettier */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Image, Pressable, StyleProp, Text, View, ViewStyle } from 'react-native';
-import VoiceToTextComponent from '../../components/VTT';
+// import VoiceToTextComponent from '../../components/VTT';
 import TextToVoiceComponent from '../../components/TTV';
+
+import Voice from '@react-native-voice/voice';
 
 
 const Myopia = (props) => {
-  const [alphabets, setAlphabets] = useState([
-    'A', 'B', 'C', 'D', 'E',
-    'F', 'G', 'H', 'I', 'J',
-    'K', 'L', 'M', 'N', 'O',
-    'P', 'Q', 'R', 'S', 'T',
-    'U', 'V', 'W', 'X', 'Y', 'Z'
-  ])
-  const [alphabet, setAlphabet] = useState(0)
-
   const backStyle: StyleProp<ViewStyle> = {
     alignItems: 'center',
     backgroundColor: 'blue',
@@ -30,11 +23,106 @@ const Myopia = (props) => {
     fontSize: 16
   };
 
-  const switchImage = () => {
-    setAlphabet(Math.floor(Math.random() * (26 - 0 + 1)) + 0)
-  }
+  const [recognizedText, setRecognizedText] = useState('');
 
-  setTimeout(switchImage, 2000)
+  const [alphabets, setAlphabets] = useState([
+    '~', 'A', 'B', 'C', 'D', 'E',
+    'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y', 'Z'
+  ])
+
+  const [resultLogos, setResultLogos] = useState([
+    require('../../images/assets/times.png'),
+    require('../../images/assets/check.png'),
+    require('../../images/assets/question_mark.png')
+  ])
+
+  const [resultLogo, setResultLogo] = useState(2)
+
+  const [alphabet, setAlphabet] = React.useState(0)
+  const alphabetRef = React.useRef(alphabet)
+
+  const second = 50
+  const interv = 5
+  const [time, setTime] = React.useState(props.initialValue || second);
+  const timerRef = React.useRef(time);
+
+  const [numQuest, setNumQuest] = React.useState(second / interv)
+  const numQuestRef = React.useRef(numQuest)
+
+  const [currQuest, setCurrQuest] = React.useState(second / interv)
+  const currQuestRef = React.useRef(currQuest)
+
+  useEffect(() => {
+    const startRecognition = async () => {
+      try {
+        await Voice.start('id');
+        console.log('start')
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    const stopRecognition = async () => {
+      try {
+        await Voice.stop();
+        console.log('end')
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    Voice.onSpeechResults = (event) => {
+      // console.log('Event', JSON.stringify(event))
+      try {
+        const text = event.value[0];
+        console.log(event.value[0]);
+        const parseHuruf = event.value[0].split('huruf ')
+        const previewHuruf = parseHuruf[1].toUpperCase()
+        console.log(`Yang Disebut ${previewHuruf}, yang ditampilkan ${alphabets[alphabetRef.current]}, Urutan huruf ${alphabetRef.current}`)
+        if (previewHuruf == alphabets[alphabetRef.current]) {
+          setResultLogo(1)
+        } else {
+          setResultLogo(0)
+        }
+        setRecognizedText(previewHuruf);
+      } catch (error) {
+        setRecognizedText('???')
+        setResultLogo(2)
+      }
+    };
+
+    const timerId = setInterval(() => {
+      timerRef.current -= 1;
+      if (timerRef.current == 50) {
+        setTime('Mulai')
+        setAlphabet(26)
+      }
+      if (timerRef.current % interv == 4) {
+        setAlphabet(Math.floor(Math.random() * (26 - 0 + 1)) + 0)
+        currQuestRef.current -= 1;
+        setCurrQuest(currQuestRef.current)
+        setResultLogo(2)
+        setRecognizedText('-')
+        startRecognition()
+      }
+      if (timerRef.current % interv == 1) {
+        stopRecognition()
+      }
+      if (timerRef.current < 0) {
+        clearInterval(timerId);
+      } else {
+        setTime(timerRef.current % interv);
+      }
+    }, 1000);
+    return () => {
+      clearInterval(timerId);
+    };
+  }
+    , [alphabet, alphabetRef, alphabets]);
+
 
   return (
     <View>
@@ -58,13 +146,15 @@ const Myopia = (props) => {
           height: 600,
         }}
       >
-        {/* <Image
-          source={require('../../images/myopiaTest.jpg')}
+        <Text
           style={{
-            width: '80%',
-            height: '80%'
+            color: 'black',
+            fontSize: 20,
+            marginTop: 100
           }}
-        /> */}
+        >
+          {currQuest} / {numQuest}
+        </Text>
         <Text
           style={{
             color: 'black',
@@ -73,21 +163,28 @@ const Myopia = (props) => {
         >
           {alphabets[alphabet]}
         </Text>
-      </View>
-
-      <View>
-        <Pressable
-        
+        <Text
+          style={{
+            color: 'black',
+            fontSize: 60
+          }}
         >
-          <Text
-            style={textStyle}
-          >
-            Back
-          </Text>
-        </Pressable>
+          {time}
+        </Text>
+        <Image
+          source={resultLogos[resultLogo]}
+          style={{
+            width: '10%',
+            height: '10%'
+          }}
+        />
+        <Text style={{ alignSelf: 'center', fontSize: 20, color: 'black' }}>Text Disebut:</Text>
+        <Text style={{ alignSelf: 'center', fontSize: 25, color: 'black', fontWeight: 'bold' }}>{recognizedText}</Text>
       </View>
 
-      <VoiceToTextComponent />
+
+
+      {/* <VoiceToTextComponent /> */}
       <TextToVoiceComponent />
     </View>
   )
